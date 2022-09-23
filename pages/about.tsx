8bd5f51be3f5f34926d9e22ee4c967e1ba959c5f
Codyvb <37961637+codyvb@users.noreply.github.com>
2022-09-23 01:02:06 -0600

    updated about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router'
 import Layout from 'components/Layout'
 import Head from 'next/head'
 import useCollectionStats from 'hooks/useCollectionStats'
+import Link from 'next/link'
 
 
 
@@ -25,11 +26,11 @@ const About = () => {
         {image}
       </Head>
 
-      <div className='relative flex justify-center col-span-full mt-36 '>
+      <div className='relative flex justify-center col-span-full mt-36  '>
 
       <div className='col-span-full md:w-2/3 px-10'>
 
-        <div className='col-span-full font-pixeloid text-5xl md:text-7xl'>
+        <div className='mt-10 col-span-full font-pixeloid text-5xl md:text-7xl'>
           <text>
         New Bros,<br></br>
         Every Week,<br></br>
@@ -41,19 +42,40 @@ const About = () => {
         New Bros
        </div> */}
 
-       <div className='bg-slate-900  mt-10 col-span-full relative p-8 justify-center text-xl font-pixeloid'>
-      A speedrun of video game history in 360 characters.
+       <div className='mt-10 mb-10 col-span-full relative justify-center text-xl font-pixeloid'>
       <div className='flex mt-4 text-lg font-pixeloid'>
         360 Total - {stats?.data?.stats?.tokenCount} / 360
       </div>
       <div className="w-full md:w-3/4 mt-4 bg-gray-200 dark:bg-gray-700 border-2">
-      <div className="bg-white h-2.5 w-[52%]" ></div>
+      <div className="bg-white h-2.5 w-[58%]" ></div>
       </div>
+
+       <Link href="https://twitter.com/gamebrosgg" target="_blank" passHref>
+       <a target="_blank">
+         <div className='mt-10 p-4 md:w-3/4 bg-white font-pixeloid col-span-full text-center text-black cursor-pointer'>
+       Next Drop Alerts
+       </div>
+       </a></Link>
 
 
       </div>
+      <div className='col-span-full font-pixeloid text-4xl md:text-5xl'>
+          <text>
+        WTF?
+        </text>
+        </div>
+
+        <div className='mt-5 col-span-full font-pixeloid text-2xl md:text-3xl md:w-3/4'>
+          <text>
+          GameBros is a speedrun of video game history in 360 characters. All bros are unique, one of one creations from the artist <Link href="https://twitter.com/bkkbros"><a target="_blank">BKKBROS</a></Link>. 
+        </text>
+        </div>
       </div>
       </div>
+
+      <div className='text-xs md:text-base mt-20 mb-20 md:mt-40 text-center font-pixeloid col-span-full relative'>
+    GameBros 2022 - Made With ❤ from Bangkok
+    </div>
 
       </Layout>
   )
